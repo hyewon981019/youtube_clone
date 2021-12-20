76fd0487d2995b664770e66f22983f9ecf6b86e2
서혜원 <hyewon981019@cau.ac.kr>
2021-12-20 18:26:00 +0900

    side 비디오 생성

--- client/src/components/views/VideoDetailPage/Section/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Section/SideVideo.js
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios';
 export default function SideVideo() {
-    return (
-        <div>
-            sideVideo
+
+    const [sideVideos, setSideVideos] = useState([])
+
+    useEffect(() => {
+        Axios.get('/api/video/getVideos')
+            .then(response=> {
+                if(response.data.success)
+                {
+                    console.log(response.data)
+                    setSideVideos(response.data.videos)
+                }
+                else{
+                    alert('비디오 가져오기 실패')
+                }
+            })
+    }, [])
+
+    const renderSideVideo = sideVideos.map((video, index) => { //어떻게 리턴이 가능하지?
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration - minutes *60));
+
+        return <div key = {index} style={{display : 'flex', marginBottom:"1rem", padding:'0 2rem'}}> 
+        <div style={{width : '40%', marginRight : '1rem'}}>
+            <a href >
+                <img style={{ width : '100%', height:'100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="썸네일" />
+            </a>
         </div>
+
+        <div style={{width : '50%'}}>
+            <a href style={{color : "gray"}}>
+                <span style={{ fontSize : '1rem', color : 'black'}}>{video.title}</span><br/>
+                <span>{video.writer.name}</span><br/>
+                <span>{video.views} views</span><br/>
+                <span>{minutes} : {seconds}</span>
+            </a>
+        </div>
+    </div>
+
+    })
+
+
+
+
+    return (
+
+        <React.Fragment>
+            <div style={{marginTop : '3rem'}}/>
+            {renderSideVideo}
+        </React.Fragment>
+
+        
     )
 }
